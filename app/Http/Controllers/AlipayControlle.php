7540ecb0163768fb423f6e740d8e61aded348939
liu <1831349921@qq.com>
2020-02-25 09:25:36 +0000

    zhifubao

--- app/Http/Controllers/AlipayControlle.php
+++ app/Http/Controllers/AlipayControlle.php
@@ -3,60 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use GuzzleHttp\Client;
+
 
 class AlipayControlle extends Controller
 {
     public function test1(){
-    	//echo __METHOD__;
-    	
-    		$client=new Client();	
 
-    	$url="https://openapi.alipaydev.com/gateway.do"; //沙箱环境
+    	   $url='https://openapi.alipaydev.com/gateway.do';
 
-    	//请求参数
-    	$common_param=[
-    	'out_trade_no'=>'test_1906'.time()."_".mt_rand(11111,99999),
-    	'product_code'=>'FAST_INSTANT_TRADE_PAY',
-    	'total_amount'=>'0.01',
-    	'subject'=>'测试订单'.mt_rand(11111,99999),
-    	];
+        $common_param=[
+            'out_trade_no'=>'1906ali_'.rand('1111',9999),
+            'product_code'=>'FAST_INSTANT_TRADE_PAY',
+            'total_amount'=>'88.88',
+            'subject'=>'测试支付'.rand(11111,99999)
+        ];
 
-    	$pub_param=[
-    	'app_id'=>env('ALIPAY_APPID'),
-    	'method'=>'alipay.trade.page.pay',
-    	'charset'=>'utf-8',
-    	'sign_type'=>'RSA2',
-    	'sign'=>'asdasd',
-    	'timestamp'=>date("Y-m-d H:i:s"),
-    	'version'=>'1.0',
-    	'biz_content'=>json_encode($common_param),
-    	];
+        $pub_param=[
+            'app_id'=>'2016101300673073',
+            'method'=>'alipay.trade.page.pay',
+            'charset'=>'utf-8',
+            'sign_type'=>'RSA2',
+            'timestamp'=>date('Y-m-d H:i:s'),
+            'version'=>'1.0',
+            'biz_content'=>json_encode($common_param,JSON_UNESCAPED_UNICODE) 
+        ];
+        $param=array_merge($common_param,$pub_param);  //合并数组
+        // 1.将签名  ASCII码递增排序
+        ksort($param);   //字典顺序排序
 
-    	$params=array_merge($common_param,$pub_param);
-    	echo "排序前";print_r($params);
-    	sort($params);
-    	echo "排序后";print_r($params);
+        // 2 拼接 url
+        $str='';
+        foreach ($param as $k=>$v){
+            $str.=$k.'='.$v.'&';
+        }
+        $str=rtrim($str,'&');
+        $request_url=$url.'?'.$str;
 
-    	$str='';
-    	foreach ($params as $k => $v) {
-    		$str .=$k . '=' . $v .'&';
-    	}
+        // 3. 算签名
+        // $keys=openssl_pkey_get_private('file://'.storage_path('keys/priv_alime.key'));
+        $keys=file_get_contents(storage_path('keys/priv_ali.key'));
 
-    	$str=rtrim($str,'&');
-		echo "待签名字符串：".$str;die;
+        $res = openssl_get_privatekey($keys);
 
-		$priv_key_id=flie_get_contents(storage_path('keys/priv_ali'));
-		openssl_sign($str,$sign,$priv_key_id,OPENSSL_ALGO_SHA256);
-		echo "签名sign".$sign;die;
-		echo "base64:".base64_encode($sign);
-		$signature=base64_encode($sign);
+        openssl_sign($str,$sign,$res,OPENSSL_ALGO_SHA256);
+        $sign=base64_encode($sign);
 
-    	$request_url=$url . $url.'?'.$str.'&sign='.urlencode($signature);
-
-    	echo "request_url:".$request_url;
-
-    	header("Location".$request_url);
+        $urls=$request_url.'&sign='.urlencode($sign);
+        // echo $urls;
+        
+        header('Location:'.$urls);
 
     }
 }
